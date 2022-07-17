06a89b50c6e8b8b95fc30ca80378bd529fbdffe6
Ayesha Zubair <zk6891745@gmail.com>
2022-07-17 10:22:51 +0530

    col

--- seeds/users.js
+++ seeds/users.js
@@ -1,5 +1,5 @@
 module.exports = [
-    "62cd64535aa1082e06bd661e",
+    "62d30a8591b72ba859c38fd1",
     "62d3014d1900bbbefdb651ff",
     "62d3035e1900bbbefdb65216",
     "62d303ed1900bbbefdb65222",
